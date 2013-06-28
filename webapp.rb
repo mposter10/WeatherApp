6ade048aa6eef8bf18a0dbb945327f4fbe052297
Marisa Poster <marisaposter@marisa-posters-macbook-666.local>
2013-06-28 13:34:23 -0400

    WeatherApp

---
+++ webapp.rb
@@ -0,0 +1,21 @@
+# Every morning at 8am, send New York City weather report to subscribers.
+
+# Use yahoo_weatherman to access http://weather.yahoo.com/united-states/new-york/new-york-2459115/
+
+# Once accessed, have Twilio send out alet message to a subscribed user.
+
+# Use Sinatra to put the app on the Web 
+
+gem install yahoo_weatherman
+
+require 'open-uri'
+
+
+event_nodes = doc.search("tbody tr")
+
+events = []
+
+event_nodes.each do |node|
+
+end
+
